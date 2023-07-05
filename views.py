4452cbdf2ed1bab80e35464b9c9e0f90355127df
Francisco Sepulveda <fsepulveda@copade.neuquen.gov.ar>
2023-07-05 14:01:26 -0300

    Crud finalizado

--- views.py
+++ views.py
@@ -1,29 +1,52 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from controlador_juegos import (
+    controller_obtener_juegos,
+    controller_obtener_juego_por_id,
+    controller_actualizar_juego,
+    controller_eliminar_juego,
+    controller_insertar_juego,
+)
 
 views = Blueprint(__name__, "views")
 
 
 @views.route("/")
 def home():
-    return render_template("index.html", name="Tim")
+    juegos = controller_obtener_juegos()
+    return render_template("index.html", juegos=juegos)
 
 
-@views.route("/profile")
-def profile():
-    return render_template("profile.html")
+@views.route("/formulario_agregar_juego")
+def form_agregar_juego():
+    return render_template("agregar_juego.html")
 
 
-@views.route("/json")
-def get_json():
-    return jsonify({"name": "tim", "coolness": 10})
+@views.route("/agregar_juego", methods=["POST"])
+def agregar_juego():
+    nombre = request.form["nombre"]
+    descripcion = request.form["descripcion"]
+    precio = request.form["precio"]
+    controller_insertar_juego(nombre, descripcion, precio)
+    return redirect(url_for("views.home"))
 
 
-@views.route("/data")
-def get_data():
-    data = request.json
-    return jsonify(data)
+@views.route("/actualizar_juego", methods=["POST"])
+def actualizar_juego():
+    id = request.form["id"]
+    nombre = request.form["nombre"]
+    descripcion = request.form["descripcion"]
+    precio = request.form["precio"]
+    controller_actualizar_juego(nombre, descripcion, precio, id)
+    return redirect(url_for("views.home"))
 
 
-@views.route("/go-to-home")
-def go_to_home():
-    return redirect(url_for("views.get_json"))
+@views.route("/formulario_editar_juego/<int:id>")
+def editar_juego(id):
+    juego = controller_obtener_juego_por_id(id)
+    return render_template("editar_juego.html", juego=juego)
+
+
+@views.route("/eliminar_juego", methods=["POST"])
+def eliminar_juego():
+    controller_eliminar_juego(request.form["id"])
+    return redirect(url_for("views.home"))
