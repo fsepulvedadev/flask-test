b947726d5819f65796b9ac00e3e5074e87bd3237
Francisco Sepulveda <fsepulveda@copade.neuquen.gov.ar>
2023-07-05 14:10:32 -0300

    Ingreso a db modificado

--- db.py
+++ db.py
@@ -2,4 +2,6 @@ import pymysql
 
 
 def obtener_conexion():
-    return pymysql.connect(host="localhost", user="root", password="", db="juegos")
+    return pymysql.connect(
+        host="localhost", user="fsepulvedadev", password="orejasnrc123", db="juegos"
+    )
